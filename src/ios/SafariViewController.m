1524795e0f54ddcee76bec2d7bf17a43eb421458
Moemen Mostafa <moemen.a1@gmail.com>
2017-06-22 14:17:07 +0400

    iOS 10 OAuth2 set cookie workaround

--- src/ios/SafariViewController.m
+++ src/ios/SafariViewController.m
@@ -111,7 +111,10 @@
 - (void) hide:(CDVInvokedUrlCommand*)command {
   if (vc != nil) {
     [vc dismissViewControllerAnimated:self.animated completion:nil];
-    vc = nil;
+      // adding a 30 sec. delay befor killing the view controller object as a work arround for iOS 10
+      dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 30 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+          vc = nil;
+      });
   }
   [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
 }
